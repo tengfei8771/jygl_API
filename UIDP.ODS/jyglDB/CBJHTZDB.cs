5bed372788f70627233c9f44dfa10c9970206c3d
zhangshe <1312719913@qq.com>
2019-09-09 10:42:37 +0800

    增加成本计划调整

--- UIDP.ODS/jyglDB/CBJHTZDB.cs
+++ UIDP.ODS/jyglDB/CBJHTZDB.cs
@@ -13,7 +13,7 @@ namespace UIDP.ODS.jyglDB
 
         public DataTable GetInfo(Dictionary<string, object> d)
         {
-            string sql = " SELECT a.S_ID,a.XMBH,a.TZQJE,a.TZJE,a.TZSM,a.TZSJ,a.CJR,a.CJSJ,b.XMMC,b.CBDW,b.TZHJHZJE  FROM jy_cbjh_adjust a left join jy_cbjh b on a.XMBH=b.XMBH  WHERE IS_DELETE=0";
+            string sql = " SELECT a.S_ID,a.XMBH,a.TZQJE,a.TZJE,a.TZSM,a.TZSJ,a.CJR,a.CJSJ,b.XMMC,b.CBDW,b.TZHJHZJE,u.USER_NAME CJRName  FROM jy_cbjh_adjust a left join jy_cbjh b on a.XMBH=b.XMBH left join ts_uidp_userinfo u on a.CJR=u.USER_ID  WHERE IS_DELETE=0";
             if (d.Keys.Contains("XMCODE") && d["XMCODE"] != null && d["XMCODE"].ToString() != "")
             {
                 sql += " and XMCODE like '%" + d["XMCODE"].ToString() + "%'";
@@ -34,28 +34,16 @@ namespace UIDP.ODS.jyglDB
         public string CreateInfo(Dictionary<string,object> d)
         {
             List<string> sqllist = new List<string>();
-            string sql = "INSERT INTO jy_fybx (S_ID,BXDH,DWBM,S_OrgCode,FYXM,XMBH,SQSJ,BXSY,BXJEDX,BXJE,YJKJE,XFKJE,FKFS,FJZS,SKDW,KHH,YHZH,SPZT,CJR,CJSJ,IS_DELETE)values(";
+            string sql = "INSERT INTO jy_cbjh_adjust (S_ID,XMBH,XMMC,TZQJE,TZJE,TZSM,TZSJ,CJR,CJSJ)values(";
             sql += GetSQLStr(Guid.NewGuid().ToString());
-            sql += GetSQLStr(d["BXDH"]);
-            sql += GetSQLStr(d["DWBM"]);
-            sql += GetSQLStr(d["S_OrgCode"]);
-            sql += GetSQLStr(d["FYXM"]);
             sql += GetSQLStr(d["XMBH"]);
-            sql += GetSQLStr(d["SQSJ"]);
-            sql += GetSQLStr(d["BXSY"]);
-            sql += GetSQLStr(d["BXJEDX"]);
-            sql += GetSQLStr(d["BXJE"], 1);
-            sql += GetSQLStr(d["YJKJE"],1);
-            sql += GetSQLStr(d["XFKJE"],1);
-            sql += GetSQLStr(d["FKFS"]);
-            sql += GetSQLStr(d["FJZS"], 1);
-            sql += GetSQLStr(d["SKDW"]);
-            sql += GetSQLStr(d["KHH"]);
-            sql += GetSQLStr(d["YHZH"]);
-            sql += GetSQLStr(0, 1);
+            sql += GetSQLStr(d["XMMC"]);
+            sql += GetSQLStr(d["TZQJE"]);
+            sql += GetSQLStr(d["TZJE"]);
+            sql += GetSQLStr(d["TZSM"]);
+            sql += GetSQLStr(d["TZSJ"]);
             sql += GetSQLStr(d["CJR"]);
             sql += GetSQLStr(DateTime.Now);
-            sql += GetSQLStr(0, 1);
             sql = sql.TrimEnd(',');
             sql += ")";
             return db.ExecutByStringResult(sql);
@@ -63,36 +51,36 @@ namespace UIDP.ODS.jyglDB
 
         public string UpdateInfo(Dictionary<string,object> d)
         {
-            string sql = " UPDATE jy_fybx SET BXDH=" + GetSQLStr(d["BXDH"]);
-            sql += "DWBM=" + GetSQLStr(d["DWBM"]);
-            sql += "S_OrgCode=" + GetSQLStr(d["S_OrgCode"]);
-            sql += "XMBH=" + GetSQLStr(d["XMBH"]);
-            sql += "FYXM=" + GetSQLStr(d["FYXM"]);
-            sql += "SQSJ=" + GetSQLStr(d["SQSJ"]);
-            sql += "BXSY=" + GetSQLStr(d["BXSY"]);
-            sql += "BXJEDX=" + GetSQLStr(d["BXJEDX"]);
-            sql += "BXJE=" + GetSQLStr(d["BXJE"], 1);
-            sql += "YJKJE=" + GetSQLStr(d["YJKJE"],1);
-            sql += "XFKJE=" + GetSQLStr(d["XFKJE"], 1);
-            sql += "FKFS=" + GetSQLStr(d["FKFS"]);
-            sql += "FJZS=" + GetSQLStr(d["FJZS"], 1);
-            sql += "SKDW=" + GetSQLStr(d["SKDW"]);
-            sql += "KHH=" + GetSQLStr(d["KHH"]);
-            sql += "YHZH=" + GetSQLStr(d["YHZH"]);
-            sql += "BJR=" + GetSQLStr(d["BJR"]);
-            sql += "BJSJ=" + GetSQLStr(DateTime.Now);
+            string sql = " UPDATE jy_cbjh_adjust SET XMBH=" + GetSQLStr(d["XMBH"]);
+            sql += "XMMC=" + GetSQLStr(d["XMMC"]);
+            sql += "TZQJE=" + GetSQLStr(d["TZQJE"]);
+            sql += "TZJE=" + GetSQLStr(d["TZJE"]);
+            sql += "TZSM=" + GetSQLStr(d["TZSM"]);
+            sql += "TZSJ=" + GetSQLStr(d["TZSJ"]);
+            //sql += "BXSY=" + GetSQLStr(d["BXSY"]);
+            //sql += "BXJEDX=" + GetSQLStr(d["BXJEDX"]);
+            //sql += "BXJE=" + GetSQLStr(d["BXJE"], 1);
+            //sql += "YJKJE=" + GetSQLStr(d["YJKJE"],1);
+            //sql += "XFKJE=" + GetSQLStr(d["XFKJE"], 1);
+            //sql += "FKFS=" + GetSQLStr(d["FKFS"]);
+            //sql += "FJZS=" + GetSQLStr(d["FJZS"], 1);
+            //sql += "SKDW=" + GetSQLStr(d["SKDW"]);
+            //sql += "KHH=" + GetSQLStr(d["KHH"]);
+            //sql += "YHZH=" + GetSQLStr(d["YHZH"]);
+            //sql += "BJR=" + GetSQLStr(d["BJR"]);
+            //sql += "BJSJ=" + GetSQLStr(DateTime.Now);
             sql = sql.TrimEnd(',');
             sql += " WHERE S_ID=" + GetSQLStr(d["S_ID"]);
             sql = sql.TrimEnd(',');
             return db.ExecutByStringResult(sql);
         }
 
-        public string DeleteInfo(Dictionary<string,object> d)
-        {
-            string sql = "UPDATE jy_fybx set IS_DELETE=1 WHERE S_ID=" + GetSQLStr(d["S_ID"]);
-            sql = sql.TrimEnd(',');
-            return db.ExecutByStringResult(sql);
-        }
+        //public string DeleteInfo(Dictionary<string,object> d)
+        //{
+        //    string sql = "UPDATE jy_fybx set IS_DELETE=1 WHERE S_ID=" + GetSQLStr(d["S_ID"]);
+        //    sql = sql.TrimEnd(',');
+        //    return db.ExecutByStringResult(sql);
+        //}
 
 
         public string GetSQLStr(object s,int flag=0)
